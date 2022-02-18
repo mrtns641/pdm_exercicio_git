407ffeb5351f134203a5945d845ab6989f260633
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-02-18 17:57:42 -0300

    Subtração implementada

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,10 @@
 public class Calculadora {
-    
+
     public double soma(double a, double b) {
         return a + b;
+    }
+
+    public double subtrair(double a, double b) {
+        return a - b;
     }
 }
--- TesteCalculadora.java
+++ TesteCalculadora.java
@@ -1,7 +1,9 @@
 public class TesteCalculadora {
     public static void main (String[] agrs) {
         Calculadora adicao = new Calculadora();
+        Calculadora subtracao = new Calculadora();
 
         System.out.println(adicao.soma(15, 8));
+        System.out.println(subtracao.subtrair(10, 5));
     }
 }
