43ae8da543c7fe69cdf1909260c70b60c59dc85a
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-02-18 17:51:52 -0300

    Classes criadas e soma implementada

---
+++ Calculadora.java
@@ -0,0 +1,6 @@
+public class Calculadora {
+    
+    public double soma(double a, double b) {
+        return a + b;
+    }
+}
---
+++ TesteCalculadora.java
@@ -0,0 +1,7 @@
+public class TesteCalculadora {
+    public static void main (String[] agrs) {
+        Calculadora adicao = new Calculadora();
+
+        System.out.println(adicao.soma(15, 8));
+    }
+}
